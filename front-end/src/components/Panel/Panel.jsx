5c8335674f44647a9b9762f61c8fef76f1b325d3
Vectari <mateusz.majer996@gmail.com>
2024-02-11 16:49:04 +0100

    update

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -4,30 +4,15 @@ import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-    const [data, setData] = useState([
-        {
-            id: 1,
-            word: "kot",
-            translation: "cat",
-            category: "noun",
-        },
-        {
-            id: 2,
-            word: "pies",
-            translation: "dog",
-            category: "noun",
-        },
-        {
-            id: 3,
-            word: "skakać",
-            translation: "jump",
-            category: "verb",
-        },
-    ]);
+    const [data, setData] = useState([]);
 
     return (
         <>
-            <Button>Załaduje dane</Button>
+            <Button onClick={() => {
+                fetch("http://localhost:3000/words").then(res => res.json()).then(res => {
+                    setData(res)
+                })
+            }}>Załaduje dane</Button>
             <section className={styles.section}>
                 <List data={data}></List>
             </section>
